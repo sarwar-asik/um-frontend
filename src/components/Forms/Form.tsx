c41fcfaae2049f684597049fa2476af8801941b0
sarwar-asik <sarwarasik@gmail.com>
2023-09-26 23:24:59 +0600

    dynamic input

--- src/components/Forms/Form.tsx
+++ src/components/Forms/Form.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React, { ReactElement, ReactNode } from "react";
 import {
   useForm,
---
+++ src/components/Forms/Forminput.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { Input } from "antd";
+import React from "react";
+import { Controller, useForm, useFormContext } from "react-hook-form";
+
+interface IInput {
+  type?: string;
+  name: string;
+  value?: string | string[] | undefined;
+  size?: `large` | "small";
+  id?: string;
+  placeholder?: string;
+  validation?: object;
+  label?: string;
+}
+
+const FormInput = ({
+  name,
+  type,
+  size,
+  id,
+  placeholder,
+  validation,
+  value,
+  label,
+}: IInput) => {
+  const { control } = useFormContext();
+
+  return (
+    <>
+      {label ? label : null}
+      <Controller
+        control={control}
+        name={name}
+        render={({ field }) => (
+          <Input
+            type={type}
+            size={size}
+            placeholder={placeholder}
+            {...field}
+            value={value ? value : field.value}
+          />
+        )}
+      />
+    </>
+  );
+};
+
+export default FormInput;
