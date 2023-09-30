85332a7c03c63a6807152ea65dcdc7405db3eb52
sarwar-asik <sarwarasik@gmail.com>
2023-09-30 14:13:41 +0600

    privateed routes

--- src/app/(withlayout)/layout.tsx
+++ src/app/(withlayout)/layout.tsx
@@ -1,15 +1,33 @@
-import React from "react";
-import { Breadcrumb, Layout, Menu, theme } from "antd";
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { Layout } from "antd";
 import SideBar from "@/components/ui/SideBar";
 import Contents from "@/components/ui/Contents";
+import { isLoggedIn } from "@/service/auth.service";
+import { useRouter } from "next/navigation";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  const userLoggedIn = isLoggedIn();
+  const router = useRouter();
+
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (!userLoggedIn) {
+      router.push("/login");
+    }
+    setIsLoading(true);
+  }, [router, userLoggedIn]);
+
+  if (!isLoading) {
+    return <p>Loading user ......</p>;
+  }
+
   return (
     <Layout hasSider>
       <SideBar></SideBar>
-     <Contents>
-     {children}
-     </Contents>
+      <Contents>{children}</Contents>
     </Layout>
   );
 };
