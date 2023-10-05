738e051b892b9f75ddc40d0557b207e8a6539e10
sarwar-asik <sarwarasik@gmail.com>
2023-10-05 22:03:23 +0600

    included stepsForm

--- src/app/(withlayout)/super_admin/manage-student/create/page.tsx
+++ src/app/(withlayout)/super_admin/manage-student/create/page.tsx
@@ -1,9 +1,25 @@
+import StepperForm from "@/components/stepperForm/stepperForm";
 import React from "react";
 
 const CreateStudentPage = () => {
+  const steps = [
+    {
+      title: "Student Information",
+      content: "Student Information-content",
+    },
+    {
+      title: "Second",
+      content: "Second-content",
+    },
+    {
+      title: "Last",
+      content: "Last-content",
+    },
+  ];
   return (
     <div>
       <h1>CreateStudentPage</h1>
+      <StepperForm steps={steps}></StepperForm>
     </div>
   );
 };
